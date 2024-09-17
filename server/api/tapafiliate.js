b0618edeafde0e0af60d262c6f20e6cecac7eedc
Ihor Huk <lsmredfilter@gmail.com>
2024-09-17 13:27:15 +0300

    configured save cookie tapfiliate click

--- server/api/tapafiliate.js
+++ server/api/tapafiliate.js
@@ -1,4 +1,6 @@
 const { serialize } = require('../api-util/sdk');
+const USING_SSL = process.env.REACT_APP_SHARETRIBE_USING_SSL === 'true';
+const CLIENT_ID = process.env.REACT_APP_SHARETRIBE_SDK_CLIENT_ID;
 
 exports.actionTapfiliate = (req, res) => {
   const query = req.body;
@@ -6,28 +8,51 @@ exports.actionTapfiliate = (req, res) => {
   fetch('https://api.tapfiliate.com/1.6/clicks/', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Api-Key': '62889eeac02707ad34511538138150493597419d'
+      "Content-Type": "application/json",
+      "Api-Key": "62889eeac02707ad34511538138150493597419d"
     },
     body: JSON.stringify({
-      referral_code: 'mjdinzm',
+      "referral_code": "mjdinzm"
     })
-  })
-  .then(json => {
-    console.log('json - fetch - suc');
-    console.log(json);
-  })
-  .catch(error => {
-    console.log('error - fetch');
-    console.error(error);
-  });
+  }).then(function(response){ 
+    response.json().then(data => {
+      console.log(data);
+      const stateKey = `st-${CLIENT_ID}-tapfiliate-click`;
+      let options = {
+        maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+        httpOnly: true, // The cookie only accessible by the web server
+        signed: true, // Indicates if the cookie should be signed
+        secure: USING_SSL,
+      }
+      res.cookie(stateKey, data.id, options);
+      return res.status(200)
+      .set('Content-Type', 'application/transit+json')
+      .send(serialize({ data: data.id }))
+      .end();
+    })
 
-  return res.status(200)
-  .set('Content-Type', 'application/transit+json')
-  .send(serialize({ data: query }))
-  .end();
+  })
+  .catch(function(response){ console.log(response) })
+
+  // axios.post('https://api.tapfiliate.com/1.6/clicks/', 
+  //   JSON.stringify({
+  //   "referral_code": "mjdinzm"
+  //   }), 
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Api-Key": "62889eeac02707ad34511538138150493597419d"
+  //     }
+  //   })
+  // .then(function (response) {
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
+
+
   
-  // res.cookie(stateKey, state, cookieOpts);
   // res.cookie(codeVerifierKey, codeVerifier, cookieOpts);
 
 };
