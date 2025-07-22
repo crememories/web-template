9e9fe42c750f4b2ca16fd88e9aebd3b95417ae95
Ihor Huk <lsmredfilter@gmail.com>
2025-07-23 00:05:03 +0300

    added facebook pexel

--- public/static/scripts/facebook/facebook.js
+++ public/static/scripts/facebook/facebook.js
@@ -28,4 +28,12 @@
   fbq('init', '723653513858411');
   fbq('track', 'PageView');
   
+  if(window.document.body !== null) {
+    const img = window.document.createElement('img');
+    img.height = '1';
+    img.width = '1';
+    img.style.display = 'none';
+    img.src = `https://www.facebook.com/tr?id=723653513858411&ev=PageView&noscript=1`;
+    window.document.body.appendChild(img);
+  }
 })();
